f15be0cd0bb38346b595a10d683d3c7564887065
Thanh <vienthanh@gmail.com>
2023-02-05 20:03:40 +0700

    crawPage function

--- crawler.js
+++ crawler.js
@@ -1,5 +1,43 @@
 const {JSDOM} = require('jsdom')
 
+async function crawPage(baseURL, currentURL, pages) {
+    console.log(`craw at ${currentURL}`)
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+
+    if(baseURLObj.hostname !== currentURLObj.hostname) return pages;
+    const normCurrentURL = normalizeURL(currentURL)
+    if(pages[normCurrentURL] > 0) {
+        pages[normCurrentURL]++
+        return pages
+    }
+
+    pages[normCurrentURL] = 1;
+
+    try {
+        const res = await fetch(currentURL)
+
+        if(res.status > 399) {
+            console.log(`error with status code ${res.status} on page ${currentURL}`)
+            return pages
+        }
+        const contentType = res.headers.get("content-type")
+        if(!contentType.includes('text/html')) {
+            console.log(`content type ${contentType}`)
+            return pages
+        }
+        const htmlText = await res.text()
+        const urls = getULRsFromHtml(htmlText, baseURL)
+        for(const url of urls) {
+            pages = await crawPage(baseURL, url, pages)
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
+
+    return await pages;
+}
+
 function getULRsFromHtml(htmlText, baseULR) {
     const dom = new JSDOM(htmlText)
     const urls = []
@@ -35,5 +73,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getULRsFromHtml
+    getULRsFromHtml,
+    crawPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+const {crawPage} = require('./crawler.js')
+
+async function main() {
+    if(process.argv.length < 3) {
+        console.log("no website provided")
+        process.exit(1);
+    }
+    if(process.argv.length > 3) {
+        console.log("too many argument provided")
+        process.exit(1)
+    }
+    const currentURL = process.argv[2]
+    const pages = await crawPage(currentURL, currentURL, {})
+
+    console.log(pages)
+}
+
+main()
